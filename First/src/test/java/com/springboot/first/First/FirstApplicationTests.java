a79bfe61040d04ec0e7253b31f3c301f39b9da4b
Prince <banerjee.prince69@gmail.com>
2024-08-18 02:49:00 +0530

    Initial Commit

---
+++ First/src/test/java/com/springboot/first/First/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.first.First;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
